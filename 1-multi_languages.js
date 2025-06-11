dfe10af93020b28f9c5e9fe37f9370b0443acda9
shimeih <shimeih@yahoo.com>
2025-06-11 17:02:32 +0200

    another correction

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const line1= "C is fun";
-=======
-const line1= "C is for fun";
->>>>>>> e54e3dbd5a3f90b287d9508b0186bb0f7820d308
 const line2= "Python is cool";
 const line3= "JavaScript is amazing";
 
